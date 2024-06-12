4ee777c31a7d52b61c411907643af5af796e7a03
Denis Georgiev <d-georgiev-91@users.noreply.github.com>
2024-06-12 18:49:26 +0300

    Drop this commit later

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # config/initializers/sidekiq.rb
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: 'redis://127.0.0.1:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: 'redis://127.0.0.1:6379/0' }
 end
